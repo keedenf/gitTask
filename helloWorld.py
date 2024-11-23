8df424800356d65194b4575776ef2fc69ba49d25
Keeden Farmer <keedenhome@Keeden-Mac.local>
2024-11-23 15:37:56 +0200

    Changes program to take and display user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,3 @@
-print("\nGit is awesome!\n")
+user_name = input('\nWhat is your name: ')
+user_colour = input('What is your favourite colour: ')
+print(f'Good day {user_name}, your favourite colour is {user_colour}\n')
