f2480f7113fb54715001c5e5287362c904074b88
Keeden Farmer <keedenhome@Keeden-Mac.local>
2024-11-27 19:39:28 +0200

    Implemented function to check inputs are not NULL

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,13 @@
-user_name = input('\nWhat is your name: ')
-user_colour = input('What is your favourite colour: ')
+def input_not_null(prompt):
+    '''Checks that user input is not NULL'''
+    user_input = input(prompt)
+    while user_input == "":
+        print("Please input a word.")
+        user_input = input(prompt)
+        return user_input
+    return user_input
+
+
+user_name = input_not_null('\nWhat is your name: ')
+user_colour = input_not_null('What is your favourite colour: ')
 print(f'Good day {user_name}, your favourite colour is {user_colour}\n')
