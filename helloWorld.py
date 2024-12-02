9ffdd6a24a7ae9d091d8bb157cbe246e6c54cbb6
Keeden Farmer <keedenhome@Keeden-Mac.local>
2024-12-02 07:53:19 +0200

    Fixed smelly code.

--- helloWorld.py
+++ helloWorld.py
@@ -1,13 +1,14 @@
 def input_not_null(prompt):
     '''Checks that user input is not NULL or is a number'''
-    user_input = input(prompt)
-    while user_input == "" or user_input.isdigit():
-        print("Please input a word.")
+    while True:
         user_input = input(prompt)
-    return user_input
+        if user_input == "" or user_input.isdigit():
+            print("Please input a word.")
+            continue
+        return user_input
 
 
 # Prompt user for name and favourite colour, then print them.
 user_name = input_not_null('\nWhat is your name: ')
-user_colour = input_not_null('What is your favourite colour: ')
-print(f'Good day {user_name}, your favourite colour is {user_colour}\n')
+user_colour = input_not_null('\nWhat is your favourite colour: ')
+print(f'\nGood day {user_name}, your favourite colour is {user_colour}\n')
